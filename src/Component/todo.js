e5ad942e0551c36caa91ae01a0d29da3d185f010
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-07-08 15:05:46 +0545

    todo lists rendered to UI

--- src/Component/todo.js
+++ src/Component/todo.js
@@ -1,15 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './todo.css'
 
-class Todo extends Component{
-    render(){
+class Todo extends Component {
+
+    state={
+        lists:[],
+        text:''
+    };
+
+    handleSubmit = (e) => {
+        
+        if(this.state.text.length===0){
+            return;
+        }
+        const newList = {
+            text:this.state.text,
+            id:Date.now()
+        }
+        this.setState(state => ({
+            lists: state.lists.concat(newList),
+            text: ''
+        }));
+        e.preventDefault();
+        e.target.querySelector('.input').value=''
+        
+    }
+    handleChange=(e)=>{
+        this.setState({text:e.target.value});
+        
+    }
+    render() {
         return (
             <div className='Todo'>
                 <div className='header-image'>
                     <h2>To-Do App</h2>
                 </div>
                 <div className='body'>
-                    <input placeholder='Add a task...' className='input' />
+                    <form onSubmit={this.handleSubmit}>
+                        <input placeholder='Add a task...' 
+                        className='input' 
+                        onChange={this.handleChange} />
+                    </form>
+                    
+                    <ol className='lists'>
+                        {this.state.lists.map(list=>(
+                            <li className='list' key={list.id}> {list.text} </li>
+                        ))}
+                    </ol>
                 </div>
 
             </div>
